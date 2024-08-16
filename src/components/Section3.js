d7cde1000e72c8abf1b42fd8b59f165461633495
Mohd Kaif <114290224+MohdKaif1729@users.noreply.github.com>
2024-08-16 19:01:14 +0530

    section3 is done

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -2,11 +2,30 @@ import React from 'react'
 
 function Section3() {
   return (
-    <section className='flex flex-col gap-5 items-center'>
-      <h2 className='font-bold text-4xl'>Services</h2>
-      <p>Lorem ipsum dolor sit amet consectetur. Tristique amet sed massa nibh lectus netus in. Aliquet donec morbi convallis pretium</p>
-      <div>
-        <div>card</div>
+    <section className='flex flex-col gap-5 items-center justify-center mt-20'>
+      <h2 className='font-semibold text-5xl'>Services</h2>
+      <p className='text-center w-50%'>Lorem ipsum dolor sit amet consectetur. Tristique amet sed massa nibh lectus netus in. Aliquet donec morbi convallis pretium</p>
+      <div className='w-[100%] flex justify-around'>
+        <div className='w-[210px] bg-[#F8F8F8] px-4 py-8 flex flex-col gap-3 rounded-md'>
+          <img src="/webdesign.svg" alt="" width={50} height={30} />
+          <h3 className='font-semibold text-2xl'>UI/UX</h3>
+          <p>Lorem ipsum dolor sit amet consectetur. Morbi diam nisi nam diam interdum</p>
+        </div>
+        <div className='w-[210px] bg-[#F8F8F8] px-4 py-8 flex flex-col gap-3 rounded-md'>
+          <img src="/webdesign.svg" alt="" width={50} height={30} />
+          <h3 className='font-semibold text-2xl'>Web Design</h3>
+          <p>Lorem ipsum dolor sit amet consectetur. Morbi diam nisi nam diam interdum</p>
+        </div>
+        <div className='w-[210px] bg-[#F8F8F8] px-4 py-8 flex flex-col gap-3 rounded-md'>
+          <img src="/graphicdesign.svg" alt="" width={50} height={30} />
+          <h3 className='font-semibold text-2xl'>App Design</h3>
+          <p>Lorem ipsum dolor sit amet consectetur. Morbi diam nisi nam diam interdum</p>
+        </div>
+        <div className='w-[210px] bg-[#F8F8F8] px-4 py-8 flex flex-col gap-3 rounded-md'>
+          <img src="/graphicdesign.svg" alt="" width={50} height={30} />
+          <h3 className='font-semibold text-2xl'>Graphic Design</h3>
+          <p>Lorem ipsum dolor sit amet consectetur. Morbi diam nisi nam diam interdum</p>
+        </div>
       </div>
     </section>
   )
