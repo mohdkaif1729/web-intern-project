35a1883b81fb073a37b41aa8cb107efc661c18a7
Mohd Kaif <114290224+MohdKaif1729@users.noreply.github.com>
2024-08-16 18:39:52 +0530

    section2 is done

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Section3() {
   return (
-    <div>Section3</div>
+    <section className='flex flex-col gap-5 items-center'>
+      <h2 className='font-bold text-4xl'>Services</h2>
+      <p>Lorem ipsum dolor sit amet consectetur. Tristique amet sed massa nibh lectus netus in. Aliquet donec morbi convallis pretium</p>
+      <div>
+        <div>card</div>
+      </div>
+    </section>
   )
 }
 
