af5b1b3c353967c2c763fb71c480817bc00a3660
Mohd Kaif <114290224+MohdKaif1729@users.noreply.github.com>
2024-08-17 20:24:17 +0530

    dobule quotes error fix

--- src/components/Section5.js
+++ src/components/Section5.js
@@ -11,7 +11,7 @@ function Section5() {
         <div className='w-[50rem] flex justify-center items-center gap-6 bg-[#F8F8F8] px-4 py-7 rounded-md  opacity-30 relative left-[-15%]'>
           <img src="https://s3-alpha-sig.figma.com/img/ac6c/6fee/baa649092172e33f9b1e0e4eb8df495d?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dXZZ53vmGB-MU83LfG8sisQg3200USCDDM9hEPhvUWP5on1iY040sNILVeBDhfiL8fQZX926KeaKgaTVlGXMP59QFm5eO2CMy9EBUaZritbRUGma-QeTQNFCPCcakEapa17JDY7hKmkDHpGknR6nV2Kh2W5ueTs96RPIrpCjFuC2c5~fD1C46Agc-YLchrLSj3DqtXw-hCcNhxy0U0YgvtrGpBQQNsaKaCCBywYeDIcN~Y1h2oEUOey6JOGAJTgAqSfTKw7in8XDK~vFd-Hm7qjgyB1y4LHbtxituuxMQuOPm7r4Y5zamlH~fvEXFnPEJHUhL9yIAdNIOZXBrkwYiw__" alt="" height={120} width={120} className='rounded-[100%]' />
           <div>
-            <p><span className='text-[#FD6F00] text-2xl font-bold'>"</span>Lorem ipsum dolor sit amet consectetur. In enim cursus odio accumsan. Id leo urna velit neque mattis id tellus arcu condimentum. Augue dictum dolor elementum convallis dignissim malesuada commodo ultrices.<span className='text-[#FD6F00] text-2xl font-bold'>"</span></p>
+            <p><span className='text-[#FD6F00] text-2xl font-bold'>&quot;</span>Lorem ipsum dolor sit amet consectetur. In enim cursus odio accumsan. Id leo urna velit neque mattis id tellus arcu condimentum. Augue dictum dolor elementum convallis dignissim malesuada commodo ultrices.<span className='text-[#FD6F00] text-2xl font-bold'>&quot;</span></p>
             <p className='font-semibold text-xl'>Name</p>
             <p>CEO</p>
           </div>
@@ -22,7 +22,7 @@ function Section5() {
         <div className='w-[50rem] flex justify-center items-center gap-6 bg-[#F8F8F8] px-4 py-7 rounded-md'>
           <img src="https://s3-alpha-sig.figma.com/img/ac6c/6fee/baa649092172e33f9b1e0e4eb8df495d?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dXZZ53vmGB-MU83LfG8sisQg3200USCDDM9hEPhvUWP5on1iY040sNILVeBDhfiL8fQZX926KeaKgaTVlGXMP59QFm5eO2CMy9EBUaZritbRUGma-QeTQNFCPCcakEapa17JDY7hKmkDHpGknR6nV2Kh2W5ueTs96RPIrpCjFuC2c5~fD1C46Agc-YLchrLSj3DqtXw-hCcNhxy0U0YgvtrGpBQQNsaKaCCBywYeDIcN~Y1h2oEUOey6JOGAJTgAqSfTKw7in8XDK~vFd-Hm7qjgyB1y4LHbtxituuxMQuOPm7r4Y5zamlH~fvEXFnPEJHUhL9yIAdNIOZXBrkwYiw__" alt="" height={120} width={120} className='rounded-[100%]' />
           <div>
-            <p><span className='text-[#FD6F00] text-2xl font-bold'>"</span>Lorem ipsum dolor sit amet consectetur. In enim cursus odio accumsan. Id leo urna velit neque mattis id tellus arcu condimentum. Augue dictum dolor elementum convallis dignissim malesuada commodo ultrices.<span className='text-[#FD6F00] text-2xl font-bold'>"</span></p>
+            <p><span className='text-[#FD6F00] text-2xl font-bold'>&quot;</span>Lorem ipsum dolor sit amet consectetur. In enim cursus odio accumsan. Id leo urna velit neque mattis id tellus arcu condimentum. Augue dictum dolor elementum convallis dignissim malesuada commodo ultrices.<span className='text-[#FD6F00] text-2xl font-bold'>&quot;</span></p>
             <p className='font-semibold text-xl'>Name</p>
             <p>CEO</p>
           </div>
@@ -33,7 +33,7 @@ function Section5() {
         <div className='w-[50rem] flex justify-center items-center gap-6 bg-[#F8F8F8] px-4 py-7 rounded-md  opacity-30 relative right-[-15%]'>
           <img src="https://s3-alpha-sig.figma.com/img/ac6c/6fee/baa649092172e33f9b1e0e4eb8df495d?Expires=1724630400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=dXZZ53vmGB-MU83LfG8sisQg3200USCDDM9hEPhvUWP5on1iY040sNILVeBDhfiL8fQZX926KeaKgaTVlGXMP59QFm5eO2CMy9EBUaZritbRUGma-QeTQNFCPCcakEapa17JDY7hKmkDHpGknR6nV2Kh2W5ueTs96RPIrpCjFuC2c5~fD1C46Agc-YLchrLSj3DqtXw-hCcNhxy0U0YgvtrGpBQQNsaKaCCBywYeDIcN~Y1h2oEUOey6JOGAJTgAqSfTKw7in8XDK~vFd-Hm7qjgyB1y4LHbtxituuxMQuOPm7r4Y5zamlH~fvEXFnPEJHUhL9yIAdNIOZXBrkwYiw__" alt="" height={120} width={120} className='rounded-[100%]' />
           <div>
-            <p><span className='text-[#FD6F00] text-2xl font-bold'>"</span>Lorem ipsum dolor sit amet consectetur. In enim cursus odio accumsan. Id leo urna velit neque mattis id tellus arcu condimentum. Augue dictum dolor elementum convallis dignissim malesuada commodo ultrices.<span className='text-[#FD6F00] text-2xl font-bold'>"</span></p>
+            <p><span className='text-[#FD6F00] text-2xl font-bold'>&quot;</span>Lorem ipsum dolor sit amet consectetur. In enim cursus odio accumsan. Id leo urna velit neque mattis id tellus arcu condimentum. Augue dictum dolor elementum convallis dignissim malesuada commodo ultrices.<span className='text-[#FD6F00] text-2xl font-bold'>&quot;</span></p>
             <p className='font-semibold text-xl'>Name</p>
             <p>CEO</p>
           </div>
